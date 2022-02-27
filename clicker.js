d362382106237dfa9b9f030cb12085eb062d4094
AdrianGomezDev <43425907+AdrianGomezDev@users.noreply.github.com>
2022-02-27 15:22:32 -0800

    setInterval to check every 5 seconds if button element is available

--- clicker.js
+++ clicker.js
@@ -1,3 +1,8 @@
-if(document.querySelector('ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 boGviw iNPKuE')){
-  document.querySelector('ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 boGviw iNPKuE').click();
-}
+setInterval(()=> {
+  if(document.querySelector('.ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 boGviw iNPKuE')){
+
+    document.querySelector('.ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 boGviw iNPKuE').click();
+
+    console.log("points box pressed");
+  }
+} ,5000);
