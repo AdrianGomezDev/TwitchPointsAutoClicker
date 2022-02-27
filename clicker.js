76a62c65ac2e293191dfe1aaa91741af511d03dd
AdrianGomezDev <43425907+AdrianGomezDev@users.noreply.github.com>
2022-02-27 14:55:09 -0800

    added descriptions, content_scripts for twitch.tv, tries to click on button element in document

---
+++ clicker.js
@@ -0,0 +1,3 @@
+if(document.querySelector('ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 boGviw iNPKuE')){
+  document.querySelector('ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 boGviw iNPKuE').click();
+}
