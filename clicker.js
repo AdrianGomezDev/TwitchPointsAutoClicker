2e50c9800c5c772985720e0dd3f74a64ca693e12
AdrianGomezDev <43425907+AdrianGomezDev@users.noreply.github.com>
2022-02-27 16:50:23 -0800

    figured out button class name added comments

--- clicker.js
+++ clicker.js
@@ -1,8 +1,10 @@
+// script that checks in 5 second intervals
 setInterval(()=> {
-  if(document.querySelector('.ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 boGviw iNPKuE')){
+  // if points button exists then click on points button and log to console
+  if(document.querySelector('.ScCoreButton-sc-1qn4ixc-0.ScCoreButtonSuccess-sc-1qn4ixc-5.boGviw.iNPKuE')){
 
-    document.querySelector('.ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 boGviw iNPKuE').click();
+    document.querySelector('.ScCoreButton-sc-1qn4ixc-0.ScCoreButtonSuccess-sc-1qn4ixc-5.boGviw.iNPKuE').click();
 
-    console.log("points box pressed");
+    console.log("points box clicked!");
   }
 } ,5000);
